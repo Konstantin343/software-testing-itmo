dfe99e8b197d407e22190a03ee2f22ced0ea5bd3
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2021-11-14 18:33:56 +0300

    Refactor e2e test

--- client/src/tests/e2e/PlacesList.spec.mjs
+++ client/src/tests/e2e/PlacesList.spec.mjs
@@ -1,68 +1,59 @@
 import {test, expect} from '@playwright/test';
-import clearDb from "./Utils.mjs";
+import {addNewList, addNewPlace, submitCredentials} from "./Steps.mjs";
 
-test.afterEach(async () => {
-    await clearDb()
-})
-
-test.beforeAll(async () => {
-    await clearDb()
-})
-
-test.beforeEach(async ({page}) => {
-    await page.goto('http://localhost:3000/sign-up')
-    await page.fill('input[name=\'login\']', 'test')
-    await page.fill('input[name=\'password\']', 'test')
-    const signUpButton = await page.locator('input[type=\'submit\']')
-    await signUpButton.click()
-})
+let suffix
+let login
+let password
 
 test.describe('Places lists', () => {
+    test.beforeEach(async ({page}) => {
+        if (!login && !password) {
+            login = 'login' + Math.random()
+            password = 'password' + Math.random()
+            await page.goto('http://localhost:3000/sign-up')
+        } else {
+            await page.goto('http://localhost:3000/sign-in')
+        }
+        suffix = Math.random()
+        await submitCredentials(page, login, password)
+    })
+
     test('Add new places list', async ({page}) => {
-        await page.goto('http://localhost:3000/sign-in')
-        await page.fill('input[name=\'login\']', 'test')
-        await page.fill('input[name=\'password\']', 'test')
-        const signInButton = await page.locator('input[type=\'submit\']')
-        await signInButton.click()
+        await addNewList(page, 'testName' + suffix, 'testDescription' + suffix)
 
-        await page.click("text=Add New List")
-        await page.fill('input[name=\'name\']', 'testName')
-        await page.fill('textarea[name=\'description\']', 'testDescription')
-        const addButton = await page.locator('button')
-        await addButton.click()
-
-        await expect(await page.locator('text=testName')).toHaveText('testName')
-        await expect(await page.locator('text=testDescription')).toHaveText('testDescription')
+        await expect(await page.locator('text=testName' + suffix))
+            .toHaveText('testName' + suffix)
+        await expect(await page.locator('text=testDescription' + suffix))
+            .toHaveText('testDescription' + suffix)
     })
 
     test('Add new places list with place', async ({page}) => {
+        suffix = Math.random()
         await page.goto('http://localhost:3000/sign-in')
-        await page.fill('input[name=\'login\']', 'test')
-        await page.fill('input[name=\'password\']', 'test')
-        const signInButton = await page.locator('input[type=\'submit\']')
-        await signInButton.click()
+        await submitCredentials(page, 'login' + suffix, 'password' + suffix)
 
-        await page.click("text=Add New List")
-        await page.fill('input[name=\'name\']', 'testName')
-        await page.fill('textarea[name=\'description\']', 'testDescription')
-        let addButton = await page.locator('button')
-        await addButton.click()
+        await addNewList(page, 'testName' + suffix, 'testDescription' + suffix)
 
-        await page.click('text=testName')
+        await page.click('text=testName' + suffix)
         await expect(await page.locator('h3')).toHaveText('Add Place')
-        await page.fill('input[name=\'name\']', 'placeTestName')
-        await page.fill('textarea[name=\'description\']', 'placeTestDescription')
-        await page.fill('input[name=\'type\']', 'placeTestType')
-        await page.fill('input[name=\'city\']', 'placeTestCity')
-        await page.fill('input[name=\'street\']', 'placeTestStreet')
-        await page.fill('input[name=\'number\']', 'placeTestNumber')
-        addButton = await page.locator('button')
-        await addButton.click()
 
-        await expect(await page.locator('text=placeTestName')).toHaveText('placeTestName')
-        await expect(await page.locator('text=placeTestDescription')).toHaveText('placeTestDescription')
-        await expect(await page.locator('text=placeTestType')).toHaveText('placeTestType')
-        await expect(await page.locator('text=placeTestCity, placeTestStreet, placeTestNumber'))
-            .toHaveText('placeTestCity, placeTestStreet, placeTestNumber')
+        await addNewPlace(
+            page,
+            'placeTestName' + suffix,
+            'placeTestDescription' + suffix,
+            'placeTestType' + suffix,
+            'placeTestCity' + suffix,
+            'placeTestStreet' + suffix,
+            'placeTestNumber' + suffix
+        )
+
+        await expect(await page.locator('text=placeTestName' + suffix))
+            .toHaveText('placeTestName' + suffix)
+        await expect(await page.locator('text=placeTestDescription' + suffix))
+            .toHaveText('placeTestDescription' + suffix)
+        await expect(await page.locator('text=placeTestType' + suffix))
+            .toHaveText('placeTestType' + suffix)
+        await expect(await page.locator('text=placeTestCity' + suffix + ', placeTestStreet' + suffix + ', placeTestNumber' + suffix))
+            .toHaveText('placeTestCity' + suffix + ', placeTestStreet' + suffix + ', placeTestNumber' + suffix)
     })
 })
