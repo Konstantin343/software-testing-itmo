72ebf5a9bd1c0c29e696cf711e0730c044748a66
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2021-12-15 13:36:58 +0300

    Test many builds

--- selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/AuthorizationTest.kt
+++ selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/AuthorizationTest.kt
@@ -1,20 +1,28 @@
 package com.itmo.kkrukhmalev.places.tests
 
+import com.codeborne.selenide.Configuration
+import com.codeborne.selenide.Selenide.open
 import com.itmo.kkrukhmalev.places.tests.utils.Steps
 import com.itmo.kkrukhmalev.places.tests.utils.UserUtils
 import org.junit.jupiter.api.Test
 
-class AuthorizationTest : BaseSelenideTest() {
+class AuthorizationTest {
+
     @Test
+    fun a() {
+        open("https://google.com")
+    }
+
+//    @Test
     fun testSignUpAndSignIn() {
         val user = UserUtils.createRandomUser()
 
-        openRelative("/sign-up")
+        open("/sign-up")
         Steps.sendCredentials(user)
         Steps.checkHelloText(user)
         Steps.signOut(user)
-        
-        openRelative("/sign-in")
+
+        open("/sign-in")
         Steps.sendCredentials(user)
         Steps.checkHelloText(user)
         Steps.signOut(user)
--- selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/PlacesTest.kt
+++ selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/PlacesTest.kt
@@ -2,23 +2,24 @@ package com.itmo.kkrukhmalev.places.tests
 
 import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Selenide.`$`
+import com.codeborne.selenide.Selenide.open
 import com.itmo.kkrukhmalev.places.tests.utils.PlacesUtils
 import com.itmo.kkrukhmalev.places.tests.utils.Steps
 import com.itmo.kkrukhmalev.places.tests.utils.UserUtils
 import org.junit.jupiter.api.Test
 
-class PlacesTest : BaseSelenideTest() {
-    @Test
+class PlacesTest {
+    //    @Test
     fun testCreatePlacesList() {
         val user = UserUtils.createRandomUser()
         val placesList = PlacesUtils.createRandomPlacesList(user)
-        
-        openRelative("/sign-up")
+
+        open("/sign-up")
         Steps.sendCredentials(user)
-        
-        openRelative("/add-places-list")
+
+        open("/add-places-list")
         Steps.createPlacesList(placesList)
-        
+
         Steps.openFirstPlacesList()
         `$`("h3").shouldHave(text("Add Place"))
     }
