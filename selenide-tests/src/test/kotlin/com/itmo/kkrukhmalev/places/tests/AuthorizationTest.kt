e2d01e4d1f8a1fb157ac3a8345628e2fd23c846b
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2021-12-15 15:10:42 +0300

    Add allure reporting

--- selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/AuthorizationTest.kt
+++ selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/AuthorizationTest.kt
@@ -1,20 +1,25 @@
 package com.itmo.kkrukhmalev.places.tests
 
-import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.Selenide.open
+import com.itmo.kkrukhmalev.places.tests.utils.BrowserDisplayNameGenerator
 import com.itmo.kkrukhmalev.places.tests.utils.Steps
 import com.itmo.kkrukhmalev.places.tests.utils.UserUtils
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.Story
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.Test
 
+@Epic("Selenide Tests")
+@Feature("Authorization Tests")
+@DisplayName("Selenide Authorization Tests (E2E)")
+
+@DisplayNameGeneration(BrowserDisplayNameGenerator::class)
 class AuthorizationTest {
-
     @Test
-    fun a() {
-        open("https://google.com")
-    }
-
-//    @Test
-    fun testSignUpAndSignIn() {
+    @Story("Sign up and sign in")
+    fun `Sign up and sign in`() {
         val user = UserUtils.createRandomUser()
 
         open("/sign-up")
--- selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/PlacesTest.kt
+++ selenide-tests/src/test/kotlin/com/itmo/kkrukhmalev/places/tests/PlacesTest.kt
@@ -3,14 +3,25 @@ package com.itmo.kkrukhmalev.places.tests
 import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Selenide.`$`
 import com.codeborne.selenide.Selenide.open
+import com.itmo.kkrukhmalev.places.tests.utils.BrowserDisplayNameGenerator
 import com.itmo.kkrukhmalev.places.tests.utils.PlacesUtils
 import com.itmo.kkrukhmalev.places.tests.utils.Steps
 import com.itmo.kkrukhmalev.places.tests.utils.UserUtils
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.Story
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.Test
 
+@Epic("Selenide Tests")
+@Feature("Places Tests")
+@DisplayName("Selenide Places Tests (E2E)")
+@DisplayNameGeneration(BrowserDisplayNameGenerator::class)
 class PlacesTest {
-    //    @Test
-    fun testCreatePlacesList() {
+    @Test
+    @Story("Sign up and create places list")
+    fun `Sign up and create places list`() {
         val user = UserUtils.createRandomUser()
         val placesList = PlacesUtils.createRandomPlacesList(user)
 
